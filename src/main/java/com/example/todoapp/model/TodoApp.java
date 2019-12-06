a349406c3d040bb31117cd1ca7e22a0343fc5ab9
smartpro1 <akeni.promise@yahoo.com>
2019-12-06 10:33:57 +0100

    add annotations to TodoApp model

--- src/main/java/com/example/todoapp/model/TodoApp.java
+++ src/main/java/com/example/todoapp/model/TodoApp.java
@@ -1,15 +1,33 @@
 package com.example.todoapp.model;
 
+import jdk.jfr.Unsigned;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.sql.Time;
 
 public class TodoApp {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
         private int id;
         private String title;
         private String description;
         private StatusEnum status;
+
+        @CreationTimestamp
+        @Column(name = "created_at")
         private Time createdAt;
+
+        @UpdateTimestamp
+        @Column(name = "updated_at")
         private Time updatedAt;
+
+        @Column(name = "completed_at")
         private Time completedAt;
 
     public int getId() {
